4a560840406c21453de416097e593d0ab7a6fb82
Aslam Hossain <aslam.ece.hstu@gmail.com>
2024-11-11 20:21:48 +0600

    Initial commit

---
+++ src/main/java/com/elk/elk/config/controller/ElkController.java
@@ -0,0 +1,28 @@
+package com.elk.elk.config.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Md. Aslam Hossain
+ * @Date Nov 10, 2024
+ */
+@Slf4j
+@RestController
+@RequestMapping("/elk")
+@AllArgsConstructor(onConstructor_ = {@Autowired})
+public class ElkController {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping("")
+    private void elk(@RequestBody String message) {
+        log.info("ELK api is call with message: " + message);
+        kafkaTemplate.send("logs", message);
+    }
+}
